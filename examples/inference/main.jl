637a616e1d0d9670abb0a2dea3863adc64d5239b
GiggleLiu <cacate0129@gmail.com>
2025-07-08 00:50:47 +0800

    initial

---
+++ examples/inference/main.jl
@@ -0,0 +1,22 @@
+using TensorInference, OMEinsumContractionOrders
+
+function main(optimizer; folder=nothing)
+    @info "Running inference with optimizer: $(optimizer)"
+    problems = dataset_from_artifact("uai2014")["MAR"]
+    problem_set_name = "relational"
+    tamaki = [251, 3, 8, 101, 11]
+    selected_ids = [3]
+    results = []
+    for (id, problem) in problems[problem_set_name]
+        if id ∉ selected_ids
+        @info "Testing: $(problem_set_name)_$id"
+        time_elapsed = @elapsed tn = TensorNetworkModel(read_model(problem); optimizer, evidence=read_evidence(problem))
+        folder !== nothing && TensorInference.OMEinsum.writejson(joinpath(folder, "$(problem_set_name)_$(id).json"), tn.code)
+        # does not optimize over open vertices
+        @info "Contraction complexity: $(contraction_complexity(tn)) (tamaki tw = $(tamaki[id])), time cost: $(time_elapsed)s"
+        push!(results, (id, contraction_complexity(tn), tamaki[id], time_elapsed))
+    end
+    return results
+end
+
+main(GreedyMethod(); folder=joinpath(@__DIR__, "samples"))
---
+++ examples/qec/main.jl
@@ -0,0 +1,15 @@
+using TensorQEC, OMEinsumContractionOrders
+
+function main(optimizer; filename=nothing)
+    @info "Running QEC with optimizer: $(optimizer)"
+    for d in [9, 13, 17, 21]
+        @info "Running surface code for d = $d"
+        tanner = CSSTannerGraph(SurfaceCode(d, d))
+        time_elapsed = @elapsed ct = compile(TNMAP(; optimizer),tanner)
+
+        # NOTE: TreeSA gives sc = 16, tc=21.74 for d = 9
+        @info "Contraction complexity: $(contraction_complexity(ct.cd.net)), time cost: $(time_elapsed)s"
+        filename !== nothing && TensorQEC.OMEinsum.writejson(filename, ct.cd.net.code)
+        return contraction_complexity(ct.cd.net)
+    end
+end
