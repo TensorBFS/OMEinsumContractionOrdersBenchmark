f74ec9a3bd96e52ccbb24fe36a7b0a465ea27450
GiggleLiu <cacate0129@gmail.com>
2025-11-08 10:41:59 +0800

    update

--- scan.sh
+++ scan.sh
@@ -12,18 +12,18 @@ SCSCORE='ScoreFunction(tc_weight=0, sc_weight=1, rw_weight=0)'
 # optimizer="KaHyParBipartite(; sc_target=25)" problem_list="$PROBLEM" make run1
 # optimizer="KaHyParBipartite(; sc_target=25, imbalances=0.0:0.1:0.8)" problem_list="$PROBLEM" make run1
 # optimizer="HyperND(; dis = METISND(), score=$TCSCORE, width=120, imbalances=130:1:130)" problem_list="$PROBLEM" make run1
-# optimizer="HyperND(; dis = KaHyParND(), score=$TCSCORE, width=120, imbalances=130:130)" problem_list="$PROBLEM" make run1
+# optimizer="HyperND(; dis = KaHyParND(), score=$TCSCORE, width=120, imbalances=130:1:130)" problem_list="$PROBLEM" make run1
 # optimizer="HyperND(; dis=METISND(), score=$TCSCORE, width=50, imbalances=100:10:800)" problem_list="$PROBLEM" make run1
 # optimizer="HyperND(; dis=KaHyParND(), score=$TCSCORE, width=50, imbalances=100:10:800)" problem_list="$PROBLEM" make run1
 # optimizer="HyperND(; dis=METISND(), score=$SCSCORE, width=120, imbalances=130:1:130)" problem_list="$PROBLEM" make run1
-# optimizer="HyperND(; dis=KaHyParND(), score=$SCSCORE, width=120, imbalances=130:130)" problem_list="$PROBLEM" make run1
+# optimizer="HyperND(; dis=KaHyParND(), score=$SCSCORE, width=120, imbalances=130:1:130)" problem_list="$PROBLEM" make run1
 # optimizer="HyperND(; dis=METISND(), score=$SCSCORE, width=50, imbalances=100:10:800)" problem_list="$PROBLEM" make run1
 # optimizer="HyperND(; dis=KaHyParND(), score=$SCSCORE, width=50, imbalances=100:10:800)" problem_list="$PROBLEM" make run1
 
-# for niters in 1 2 4 6 8 10 20 30 40 50; do 
-#     optimizer="TreeSA(niters=$niters, score=$TCSCORE)" problem_list="$PROBLEM" make run1
-#     optimizer="TreeSA(niters=$niters, score=$SCSCORE)" problem_list="$PROBLEM" make run1
-# done
+for niters in 1 2 4 6 8 10 20 30 40 50; do 
+    optimizer="TreeSA(niters=$niters, score=$TCSCORE)" problem_list="$PROBLEM" make run1
+    optimizer="TreeSA(niters=$niters, score=$SCSCORE)" problem_list="$PROBLEM" make run1
+done
 
 # for niters in {0..10}; do 
 #     alpha=$(echo "scale=1; $niters * 0.1" | bc)
@@ -39,10 +39,10 @@ COTENGRA_PROBLEMS="[['quantumcircuit', 'sycamore_53_20_0.json']]"
 #     method=greedy params="{'max_repeats': $n, 'problems': $COTENGRA_PROBLEMS, 'minimize': 'size'}" make run1-cotengra
 # done
 
-for imb in 0.01 0.1 0.3 0.5 0.8; do
-    method=kahypar params="{'imbalance': $imb, 'problems': $COTENGRA_PROBLEMS, 'minimize': 'flops'}" make run1-cotengra
-    method=kahypar params="{'imbalance': $imb, 'problems': $COTENGRA_PROBLEMS, 'minimize': 'size'}" make run1-cotengra
-done
+# for imb in 0.01 0.1 0.3 0.5 0.8; do
+#     method=kahypar params="{'imbalance': $imb, 'problems': $COTENGRA_PROBLEMS, 'minimize': 'flops'}" make run1-cotengra
+#     method=kahypar params="{'imbalance': $imb, 'problems': $COTENGRA_PROBLEMS, 'minimize': 'size'}" make run1-cotengra
+# done
 
 # Generate summary and report after all benchmarks complete
 echo ""
